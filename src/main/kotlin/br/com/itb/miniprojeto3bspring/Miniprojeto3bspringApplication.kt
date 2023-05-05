92477cb071daa3638e98eb972bdd0b93e9f107a0
felipe-brito7 <philipzbrito777@gmail.com>
2023-05-05 08:27:36 -0300

    aula inicial

---
+++ src/main/kotlin/br/com/itb/miniprojeto3bspring/Miniprojeto3bspringApplication.kt
@@ -0,0 +1,11 @@
+package br.com.itb.miniprojeto3bspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Miniprojeto3bspringApplication
+
+fun main(args: Array<String>) {
+	runApplication<Miniprojeto3bspringApplication>(*args)
+}
